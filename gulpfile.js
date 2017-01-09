58da4ab9e04d6f236276debe57b9239a3b854ce3
Dmitriy Khomenkov <dmkhomen@gmail.com>
2017-01-09 22:18:14 +0200

    started app shell

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    browserSync = require('browser-sync').create(),
-    browserify = require('gulp-browserify'),
-    uglify = require('gulp-uglify'),
-    sourcemaps = require('gulp-sourcemaps'),
-    gulpif = require('gulp-if'),
-    concat = require('gulp-concat'),
-    sass = require('gulp-sass'),
-    imagemin = require('gulp-imagemin'),
-    pngcrush = require('imagemin-pngcrush'),
-    jsonminify = require('gulp-jsonminify'),
-    minifyHTML = require('gulp-minify-html'),
-    swPrecache = require('sw-precache');
+gutil = require('gulp-util'),
+browserSync = require('browser-sync').create(),
+browserify = require('gulp-browserify'),
+uglify = require('gulp-uglify'),
+sourcemaps = require('gulp-sourcemaps'),
+gulpif = require('gulp-if'),
+concat = require('gulp-concat'),
+sass = require('gulp-sass'),
+imagemin = require('gulp-imagemin'),
+pngcrush = require('imagemin-pngcrush'),
+jsonminify = require('gulp-jsonminify'),
+minifyHTML = require('gulp-minify-html'),
+swPrecache = require('sw-precache');
 
 var src = './src',
-    dist = './dist',
-    enviroment = 'production';
+dist = './dist',
+enviroment = 'production';
 
 var proxy = 'localhost/learning-progressive/dist';
 
@@ -33,64 +33,64 @@ gulp.task('browser-sync', function() {
 
 
 gulp.task('js',function(){
-  gulp.src(src + '/js/*.js')
+    gulp.src(src + '/js/app.js')
     .pipe(concat('app.js'))
     .pipe(browserify())
     .pipe(uglify())
     .pipe(gulp.dest(dist + '/js/'))
     .pipe(browserSync.reload({
-      stream: true
+        stream: true
     }))
 });
 
 gulp.task('css',function(){
-  return gulp.src(src + '/css/*.scss')
+    return gulp.src(src + '/css/*.scss')
     .pipe(sass({
-      outputStyle: 'compressed'
+        outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(gulp.dest(dist + '/css/'))
     .pipe(browserSync.reload({
-      stream: true
+        stream: true
     }))
 });
 
 gulp.task('json',function(){
-  gulp.src(src + '/data/*.json')
+    gulp.src(src + '/data/*.json')
     .pipe(jsonminify())
     .pipe(gulp.dest(dist + '/data/'))
     .pipe(browserSync.reload({
-      stream: true
+        stream: true
     }))
 });
 
 gulp.task('html',function(){
-  gulp.src(src + '/*.php')
+    gulp.src(src + '/*.php')
     .pipe(minifyHTML())
     .pipe(gulp.dest(dist))
     .pipe(browserSync.reload({
-      stream: true
+        stream: true
     }))
 });
 
 gulp.task('images',function(){
-  gulp.src(src + '/images/**/*.*')
+    gulp.src(src + '/images/**/*.*')
     .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewbox: false}],
-      use: [pngcrush()]
+        progressive: true,
+        svgoPlugins: [{removeViewbox: false}],
+        use: [pngcrush()]
     }))
     .pipe(gulp.dest(dist + '/images'))
     .pipe(browserSync.reload({
-      stream: true
+        stream: true
     }))
 });
 
 gulp.task('watch', function(){
-  gulp.watch(src + '/js/*.js', ['js']);
-  gulp.watch(src + '/css/*.scss', ['css']);
-  gulp.watch(src + '/data/*.json', ['json']);
-  gulp.watch(src + '/*.php', ['html']);
-  gulp.watch(src + '/images/**/*.*', ['images']);
+    gulp.watch(src + '/js/*.js', ['js']);
+    gulp.watch(src + '/css/*.scss', ['css']);
+    gulp.watch(src + '/data/*.json', ['json']);
+    gulp.watch(src + '/*.php', ['html']);
+    gulp.watch(src + '/images/**/*.*', ['images']);
 });
 
 gulp.task('default', ['browser-sync', 'js', 'css', 'json', 'html', 'images', 'watch']);
